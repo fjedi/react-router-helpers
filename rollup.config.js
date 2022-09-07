3181b390b059db276c026d1b0c1f7c8dec254fab
Alexander Radyushin <a.radyushin@pm.me>
2022-09-06 21:40:07 -0300

    feat: add 'browser' entrypoint

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,70 @@
 import typescript from 'rollup-plugin-typescript2';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 //
 import pkg from './package.json';
 
-// continued
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-      strict: false,
-    },
-  ],
-  plugins: [
-    // Preferably set as first plugin.
-    peerDepsExternal(),
-    //
-    nodeResolve(),
-    commonjs({
-      // namedExports: {
-      //   'node_modules/subscriptions-transport-ws/dist/index.js': ['SubscriptionClient'],
-      // },
-    }),
-    typescript({ objectHashIgnoreUnknownHack: false }),
-  ],
+const input = 'src/index.tsx';
+const commonOutputOptions = {
+  exports: 'named',
+  sourcemap: true,
+  strict: false,
 };
+const commonPluginsHead = [
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    preventAssignment: true,
+  }),
+  // Preferably set as first plugin.
+  peerDepsExternal(),
+];
+const commonPluginsMiddle = [
+  commonjs({
+    // namedExports: {
+    //   'node_modules/subscriptions-transport-ws/dist/index.js': ['SubscriptionClient'],
+    // },
+  }),
+  typescript({ objectHashIgnoreUnknownHack: false }),
+];
+
+export default [
+  {
+    input,
+    output: [
+      {
+        file: `${pkg.browser}.mjs`,
+        format: 'es',
+        ...commonOutputOptions,
+      },
+    ],
+    plugins: [
+      ...commonPluginsHead,
+      nodeResolve({ browser: true }),
+      ...commonPluginsMiddle,
+      terser({
+        ecma: 2015,
+      }),
+    ],
+  },
+  // isomorphic bundle (client+ssr)
+  {
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        ...commonOutputOptions,
+      },
+    ],
+    plugins: [
+      ...commonPluginsHead,
+      nodePolyfills(),
+      nodeResolve({ browser: false }),
+      ...commonPluginsMiddle,
+    ],
+  },
+];
